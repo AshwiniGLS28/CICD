526ff1bee591f070f2d42d28d8cd039955006ba4
AshwiniGLS28 <ashwini@giantleapsystems.com>
2023-03-16 11:42:10 +0530

    Test cases done

--- src/test/java/com/cicdDemo/controller/DataControllerTest.java
+++ src/test/java/com/cicdDemo/controller/DataControllerTest.java
@@ -1,7 +1,6 @@
 package com.cicdDemo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
@@ -14,23 +13,26 @@ public class DataControllerTest {
 
     @Test
     void health() {
+    	dataController = new DataController();
         assertEquals("HEALTH CHECK OK!", dataController.healthCheck());
     }
 
     @Test
     void version() {
+    	dataController = new DataController();
         assertEquals("The actual version is 1.0.0", dataController.version());
     }
 
     @Test
     void nationsLength() {
+    	dataController = new DataController();
         Integer nationsLength = dataController.getRandomNations().size();
         assertEquals(10, nationsLength);
-        fail();
     }
 
     @Test
     void currenciesLength() {
+    	dataController = new DataController();
         Integer currenciesLength = dataController.getRandomCurrencies().size();
         assertEquals(20, currenciesLength);
     }
